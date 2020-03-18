da4ccd42390ebb8abc9399364c2b4eda046abc98
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-03-18 17:32:55 +0545

    Add endpoints in config for trending list and baseimage

--- src/config.js
+++ src/config.js
@@ -3,13 +3,17 @@
  */
 const config = {
   baseApiURI: process.env.REACT_APP_API_URL || 'API BASE URL',
+  baseImageUrl: process.env.REACT_APP_IMAGE_URL || 'IMAGE BASE URL',
 
   apiKey: process.env.REACT_APP_API_KEY || 'Create API KEY at https://www.themoviedb.org/settings/api',
 
   endpoints: {
     tv: '/tv',
     movie: '/movie',
-    trending: '/trending'
+    trending: '/trending',
+    trendingMovie: '/trending/movie/day',
+    trendingTV: '/trending/tv/day',
+    movieGenre: '/genre/movie/list'
   }
 };
 
