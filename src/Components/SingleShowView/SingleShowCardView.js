bf598b728c6198ff9caf4aff122409d09ae5f9ab
Biplap Bhattarai <bhattaraib58@gmail.com>
2022-07-27 22:49:55 +0545

    Add exhaustive-deps in eslint

--- src/Components/SingleShowView/SingleShowCardView.js
+++ src/Components/SingleShowView/SingleShowCardView.js
@@ -83,6 +83,7 @@ export default function SingleShowCardView({ showObj, getGenre }) {
 
   useEffect(() => {
     fetchIMDBExternalDetail();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
